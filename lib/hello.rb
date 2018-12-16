b6bf43ffff850e481030f70029a4fd4581ca8174
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-12-16 21:52:51 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -7,11 +7,11 @@ i = 0
   array
 end
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
+# hello_t(["Tim", "Tom", "Jim"]) do |name|
+#   if name.start_with?("T")
+#     puts "Hi, #{name}"
+#   end
+# end
 
 
 
